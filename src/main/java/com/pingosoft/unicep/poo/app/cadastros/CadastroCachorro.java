d158382e85eff283be68cef2bac92ebebc312ddf
Fernando Kulaif <ferakulaif@gmail.com>
2017-06-12 20:05:59 -0300

    .config files from eclipse and maven pom.xml

---
+++ src/main/java/com/pingosoft/unicep/poo/app/cadastros/CadastroCachorro.java
@@ -0,0 +1,18 @@
+package com.pingosoft.unicep.poo.app.cadastros;
+
+public class CadastroCachorro {
+	public static void main(String[] args) {
+		cabecalhoCachorro();
+		cadastrarCachorro();
+	}
+
+	private static void cabecalhoCachorro() {
+		System.out.println("### --- ------------------------- --- ###");
+		System.out.println("### ---   CADASTRO DE CACHORROS   --- ###");
+		System.out.println("### --- ------------------------- --- ###");
+	}
+
+	private static void cadastrarCachorro() {
+		System.out.println("Cachorro cadastrado com sucesso!");
+	}
+}
